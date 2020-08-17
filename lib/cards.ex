f964764cde0edf197cfbf707254908d6b079ee4a
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-08-17 17:35:01 -0500

    inclusion of doctests and 'normal' tests within elixir

--- lib/cards.ex
+++ lib/cards.ex
@@ -5,6 +5,12 @@ defmodule Cards do
 
   @doc """
   Returns a list of strings representing a deck of playing cards.
+
+  ## Examples
+
+        iex> Cards.create_deck()
+        ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -19,6 +25,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Determines whether a deck contains a given card
+
+  ## Examples
+
+        iex> deck = Cards.create_deck()
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
